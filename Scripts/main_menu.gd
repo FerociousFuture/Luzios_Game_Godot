27d44391d0e4f0a4b102860715936f07cdadd60c
DenisseXocuis <denissexocuis@gmail.com>
2025-04-22 21:05:31 -0600

    Menu y cosas

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
-
-
-
 func _on_inicio_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
+
+func _on_about_pressed() -> void:
+	pass # Replace with function body.
