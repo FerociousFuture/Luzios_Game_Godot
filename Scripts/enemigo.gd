588fde1a47afcb82204e579a32ee6e0b86b24fa7
FerociousFuture <haziellopezcastillo007@hotmail.com>
2025-04-23 22:41:49 -0600

    New enemy was added

--- Scripts/enemigo.gd
+++ Scripts/enemigo.gd
@@ -2,11 +2,26 @@ extends Node2D
 
 @onready var area2d : Area2D = $Area2D
 @onready var animation : AnimationPlayer = $AnimationPlayer
-@onready var hit: AudioStreamPlayer2D = $hit
-@onready var dead: AudioStreamPlayer2D = $dead
+@onready var raycast_pared : RayCast2D = $Pared
+@onready var raycast_suelo : RayCast2D = $Suelo
 
+var velocidad := 50
+var direccion := -1
 var vida := 3
 var muerto := false
+
+func _physics_process(delta):
+	if muerto:
+		return
+	if raycast_suelo.is_colliding():
+		print("Tocando suelo")
+
+	if raycast_pared.is_colliding() or not raycast_suelo.is_colliding():
+		direccion *= -1
+		scale.x *= -1
+
+	position.x += direccion * velocidad * delta
+	animation.play("Caminar")
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if muerto:
@@ -16,7 +31,6 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		vida -= 1
 		print("Ahh me pegó!!! Vida restante:", vida)
 		animation.play("Golpeado")
-		hit.play()
 
 		if vida <= 0:
 			morir()
@@ -25,7 +39,6 @@ func morir():
 	muerto = true
 	print("¡He muerto!")
 	animation.play("Muerte")
-	dead.play()
 	await animation.animation_finished
 	queue_free()
 	print("Ya se murio en serio")
